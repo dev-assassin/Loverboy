d9a47fe790328569dd56e88659cbaa1ebd1a1778
alexandra1030 <alexandra.uefa@gmail.com>
2017-10-18 23:18:21 +0200

    add modal

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,35 @@
   <section class="hero is-light">
     <div class="container">
       <div class="hero-body">
-        <h1 class="title Graphik-Bold-Web hero-title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </h1>
+        <h1 class="title Graphik-Bold-Web hero-title">Loverboy er Danmarks største og mest besøgte online community for homo, bi og transseksuelle.</h1>
+        <button class="button is-dark is-large hero-button Graphik-Bold-Web" @click="showModal = true"><i class="fa fa-user user-icon " />&nbsp;Join today</button>
+        <div class="modal is-active" v-if="showModal">
+          <div class="modal-background"></div>
+          <div class="modal-content box">
+            <div class="tabs is-centered is-medium">
+              <ul class="register Graphik-Bold-Web">
+                <li class="is-active"><a>Join</a></li>
+                <li><a style="color:#eee">Log in</a></li>
+              </ul>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input class="input is-large" type="text" placeholder="Name">
+                <input class="input is-large" type="email" placeholder="Email">
+                <input class="input is-large" type="password" placeholder="Password">
+                <button class="button is-dark is-large" style="width:100%"><i class="fa fa-user user-icon " />&nbsp;Join</button>
+                <h1 class="signup">Sign up with</h1>
+                <div class="level">
+                  <button class="button is-info is-large level-item">Facebook</button>
+                  <button class="button is-danger is-large level-item">Google</button>
+                </div>
+                <p style="text-align:center">Some legal desclamer to get rid of mandatory<p>
+                <p style="text-align:center; margin-bottom:30px">check boxes and validation</p>
+              </div>
+            </div>
+          </div>
+          <button class="modal-close is-large" aria-label="close" @click="showModal = false"></button>
+        </div>
       </div>
     </div>
   </section>
@@ -125,6 +151,7 @@ export default {
   name: 'Home',
   data () {
     return {
+      showModal: false
     }
   }
 }
